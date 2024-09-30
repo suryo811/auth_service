64c47bb2ab8ce12206f695b58c2f65afde29983c
suryoday kaushik <suryodaykaushik@gmail.com>
2024-09-30 11:08:11 +0530

    add async handler

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
-import connectDB from './utils/db.js';
+import connectDB from './utils/db.js'
+import AppError from './utils/appError.js'
+import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js'
+import asyncHandler from './utils/asyncHandler.js'
 
 
 const app = express();
@@ -9,11 +12,15 @@ const app = express();
 
 
 
+app.all("*", (req, _, next) => {
+    throw new AppError(`Can't find ${req.originalUrl} on this server`, 404)
+});
+
+app.use(errorHandlerMiddleware)
 
 
-
+//server initialization
 const port = process.env.PORT || 3001;
-
 const start = async () => {
     try {
         await connectDB();
